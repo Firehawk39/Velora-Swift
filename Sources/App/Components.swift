85fe5b56abbb4e955621d9f14f9e2ba4c6aa08d4
Firehawk39 <jolly1506@gmail.com>
2026-05-02 11:07:37 +0530

    Fix ScreenTier to use UIDevice.userInterfaceIdiom for iPad detection instead of UIScreen.main.bounds.width which fails in compatibility mode

--- Sources/App/Components.swift
+++ Sources/App/Components.swift
@@ -5,15 +5,20 @@ import Foundation
 public enum ScreenTier {
     case tiny, compact, regular, large, huge
     public static var current: ScreenTier {
+        // On iPad hardware, always use tablet tiers regardless of compatibility scaling
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            let w = UIScreen.main.bounds.width
+            if w >= 1024 { return .huge }  // iPad Pro 12.9"
+            return .large                   // All other iPads
+        }
+        // iPhone tier detection by screen width
         let w = UIScreen.main.bounds.width
-        if w <= 320 { return .tiny } // iPhone SE 1st Gen
-        if w < 414 { return .compact } // Standard iPhone / mini
-        if w < 768 { return .regular } // Plus/Max iPhones
-        if w < 1024 { return .large } // 10.25" Displays / standard iPads
-        return .huge // iPad Pro 12.9"
+        if w <= 320 { return .tiny }    // iPhone SE 1st Gen
+        if w < 414 { return .compact }  // Standard iPhone / mini
+        return .regular                  // Plus/Max iPhones
     }
     public static var isSE: Bool { current == .tiny }
-    public static var isPhone: Bool { UIScreen.main.bounds.width < 768 }
+    public static var isPhone: Bool { UIDevice.current.userInterfaceIdiom == .phone }
     public static var isHuge: Bool { current == .huge }
 }
 
