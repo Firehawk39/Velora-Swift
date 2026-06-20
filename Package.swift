c48297cee5624b50dc20419ea66be4f147a9e156
Firehawk39 <38713571+Firehawk39@users.noreply.github.com>
2026-06-20 10:09:29 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,33 +1,45 @@
 // swift-tools-version: 6.0
 import PackageDescription
+
+#if canImport(AppleProductTypes)
 import AppleProductTypes
+
+let products: [Product] = [
+    .iOSApplication(
+        name: "Velora",
+        targets: ["AppModule"],
+        bundleIdentifier: "com.velora.aistudio",
+        teamIdentifier: "",
+        displayVersion: "1.0",
+        bundleVersion: "1",
+        appIcon: .asset("AppIcon"),
+        supportedDeviceFamilies: [
+            .pad,
+            .phone
+        ],
+        supportedInterfaceOrientations: [
+            .portrait,
+            .landscapeRight,
+            .landscapeLeft,
+            .portraitUpsideDown
+        ]
+    )
+]
+#else
+let products: [Product] = [
+    .executable(
+        name: "Velora",
+        targets: ["AppModule"]
+    )
+]
+#endif
 
 let package = Package(
     name: "Velora",
     platforms: [
         .iOS(.v15)
     ],
-    products: [
-        .iOSApplication(
-            name: "Velora",
-            targets: ["AppModule"],
-            bundleIdentifier: "com.velora.aistudio",
-            teamIdentifier: "",
-            displayVersion: "1.0",
-            bundleVersion: "1",
-            appIcon: .asset("AppIcon"),
-            supportedDeviceFamilies: [
-                .pad,
-                .phone
-            ],
-            supportedInterfaceOrientations: [
-                .portrait,
-                .landscapeRight,
-                .landscapeLeft,
-                .portraitUpsideDown
-            ]
-        )
-    ],
+    products: products,
     targets: [
         .executableTarget(
             name: "AppModule",
